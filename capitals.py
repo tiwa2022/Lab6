182c6ff28e9fe1567eceefffacfa96944ba48e13
tiwa2022 <97244287+tiwa2022@users.noreply.github.com>
2022-02-10 21:01:19 -0500

    commit

---
+++ capitals.py
@@ -0,0 +1,29 @@
+def getCap():
+    dictionary = {  
+        "BC": "Victoria",
+        "Alberta": "Edmonton"
+        "Saskatchewan" "Regina",
+        "New Brunswick": "Frederiction",
+        "Manitoba": "Winnipeg",
+        "Ontario": "Toronto",
+        "P.E.I": "Charlottetown",
+        "Nova Scotia": "Halifax",
+        "New Found Land": "St. John's"
+    }
+    return dictionary
+
+if __name__ =="__main__":
+
+    counter= 0
+    dictionary = getCap()
+    for province in dictionary.items():
+        inp = input("The province is", {province[0]},".", "What is the capital?")
+
+        if inp == province[1]:
+            print("That's right!!")
+            counter += 1
+        else:
+            print ("Nice try, The answer is", province[1])
+            print (f"Current score: {counter}/9")
+    print (f"Your score was {counter}/9")
+            
